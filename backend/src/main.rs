42f7e89e2e8753e5444b877ad155dd88510c0df0
shyu61 <ohs6261@gmail.com>
2021-04-12 01:40:03 +0900

    Initate backend

---
+++ backend/src/main.rs
@@ -0,0 +1,23 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+async fn index() -> Result<actix_files::NamedFile> {
+    Ok(actix_files::NamedFile::open("target/public/index.html")?)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+        .route("/", web::get().to(index))
+        .route("/hey", web::get().to(manual_hello))
+        .service(actix_files::Files::new("", "target/public"))
+        .default_service(web::route().to(index))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
