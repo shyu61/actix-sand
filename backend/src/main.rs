cf054508253a5a84653bdf16f83a8aa7330ed499
shyu61 <ohs6261@gmail.com>
2021-04-25 22:00:37 +0900

    Create endpoint to get result

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,14 +2,24 @@ use actix_files;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder, Result, http::header, middleware::Logger};
 use serde::{Serialize, Deserialize};
 use rusoto_core::{Region, credential::{DefaultCredentialsProvider, ProvideAwsCredentials}};
-use rusoto_s3::{PutObjectRequest, util::PreSignedRequest};
+use rusoto_s3::{GetObjectRequest, PutObjectRequest, util::PreSignedRequest, S3Client, S3};
 use dotenv::dotenv;
 use std::env;
 use actix_cors::Cors;
+use log::{info, warn};
+use regex::Regex;
+use bytes;
+use futures::stream::TryStreamExt;
 
 #[derive(Deserialize, Clone)]
 struct UploadRequeset {
     file_name: String,
+}
+
+#[derive(Serialize)]
+struct UploadResponse {
+    signed_url: String,
+    key: String,
 }
 
 async fn get_s3_presign(params: web::Json<UploadRequeset>) -> impl Responder {
@@ -17,9 +27,10 @@ async fn get_s3_presign(params: web::Json<UploadRequeset>) -> impl Responder {
 
     let bucket = env::var("BUCKET").expect("BUCKET is not found");
     let region = Region::ApNortheast1;
+    let key = format!("input/{}", params.file_name.clone());
     let req = PutObjectRequest {
         bucket: bucket,
-        key: params.file_name.clone(),
+        key: key.clone(),
         ..Default::default()
     };
     let credentials = DefaultCredentialsProvider::new()
@@ -29,7 +40,50 @@ async fn get_s3_presign(params: web::Json<UploadRequeset>) -> impl Responder {
             .unwrap();
     
     let signed_url = req.get_presigned_url(&region, &credentials, &Default::default());
-    let response = serde_json::to_string(&signed_url).unwrap();
+    let response = serde_json::to_string(&UploadResponse { signed_url: signed_url, key: key }).unwrap();
+    info!("debug! response: {}", response);
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .body(response)
+}
+
+#[derive(Deserialize)]
+struct GetTranscriptionRequest {
+    key: String,
+}
+
+async fn get_transcription(info: web::Query<GetTranscriptionRequest>) -> impl Responder {
+    dotenv().ok();
+
+    let bucket = env::var("BUCKET").expect("BUCKET is not found");
+    let region = Region::ApNortheast1;
+    let key = info.key.clone();
+    let dir = Regex::new(r"input").unwrap();
+    let ext = Regex::new(r"\..+$").unwrap();
+    let real_key = dir.replace(&key, "output");
+    let real_real_key = ext.replace(&real_key, ".txt").into_owned();
+    let req = GetObjectRequest {
+        bucket: bucket,
+        // key: real_real_key,
+        key: "output/sample.txt".to_string(),
+        ..Default::default()
+    };
+    let client = S3Client::new(region);
+    let result = client.get_object(req).await.expect("Cloudn't GET object");
+
+    let stream = result.body.unwrap();
+    let body = stream.map_ok(|b| bytes::BytesMut::from(&b[..])).try_concat().await.unwrap();
+
+    // async_readしたい場合
+    // use tokio::io::AsyncReadExt;
+    // let mut stream = result.body.unwrap().into_async_read();
+    // let mut body = Vec::new();
+    // stream.read_to_end(&mut body).await.unwrap();
+    let converted: String = String::from_utf8(body.to_vec()).unwrap();
+
+    warn!("=====BODY=====: {:?}", body);
+
+    let response = serde_json::to_string(&converted).unwrap();
     HttpResponse::Ok()
         .content_type("application/json")
         .body(response)
@@ -105,7 +159,8 @@ async fn main() -> std::io::Result<()> {
             web::scope("/api")
                 .route("/first", web::get().to(first))
                 .route("/second", web::get().to(second))
-                .route("/upload", web::post().to(get_s3_presign)),
+                .route("/upload", web::post().to(get_s3_presign))
+                .route("/transcription", web::get().to(get_transcription)),
         )
         .service(actix_files::Files::new("", "target/public"))
         .default_service(web::route().to(index))
