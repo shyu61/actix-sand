7cc0075c32e62708b49f3a4506d412fcc50caa5a
shyu61 <ohs6261@gmail.com>
2021-04-29 21:49:15 +0900

    Clear unnecessary api

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,11 @@
-import { request } from './hooks';
 import { Upload } from './component/upload';
 import styled from 'styled-components';
-import React, { useEffect, useState } from 'react';
-
-type SecondData = {
-  list: {
-    id: number,
-    name: string
-  }[]
-}
-
-const initialSecondData = {
-  list: [{
-    id: 0,
-    name: 'sample',
-  }]
-}
+import React from 'react';
 
 function App() {
-  const [firstData, setFirstData] = useState<string[]>([]);
-  const [secondData, setSecondData] = useState<SecondData>(initialSecondData);
-
-  const getFirstData = async () => {
-    const res = await request<string[]>({ method: 'GET', path: '/api/first' });
-    setFirstData(res);
-  }
-
-  const getSecondData = async () => {
-    const res = await request<SecondData>({ method: 'GET', path: '/api/second' });
-    setSecondData(res);
-  }
-
-  useEffect(() => {
-    getFirstData();
-    getSecondData();
-  }, []);
-
   return (
     <StyledContainer>
-      <h2>First Data</h2>
-      {firstData.map(item => <p>{item}</p>)}
-      <h2>Second Data</h2>
-      {secondData['list'].map(item => <p>Id: {item.id}, Name: {item.name}</p>)}
+      <StyledTitle>Transcripter</StyledTitle>
       <Upload />
     </StyledContainer>
   );
@@ -51,5 +15,10 @@ export default App;
 
 const StyledContainer = styled.div`
   text-align: center;
-  margin: 0 50px;
+  margin: 100px 50px;
+`;
+
+const StyledTitle = styled.h2`
+  border-bottom: 2px solid darkgray;
+  margin: 30px 400px;
 `;
